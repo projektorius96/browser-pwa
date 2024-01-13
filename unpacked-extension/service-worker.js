1a852f9ed26f24f9db7ccda239a67b2eddbdcd08
LG <34185919+projektorius96@users.noreply.github.com>
2024-01-13 17:00:09 +0200

    working draft with caveats, see desc.
    - minimising seems to reflect each state incrementally, I want to keep commit linearly, but will tweak this buggy intermediate minimizing behaviour within the next commit

--- unpacked-extension/service-worker.js
+++ unpacked-extension/service-worker.js
@@ -4,6 +4,10 @@
 IF CURRENTLY WINDOW IS  |   MINIMIZED     | "normal" (or "minimized" if .getCurrent was called from console)
 IF CURRENTLY WINDOW IS  |   MAXIMIZED     | "maximized"
 IF CURRENTLY WINDOW IS  |   FULLSCREEN    | "fullscreen"
+
+OTHER CONSIDERATIONS:
+- in PWA FULLSCREEN MODE, FULLSCREEN IS EQUIVALENT TO MAXIMIZED, however (see next line)
+-- if I leave FULLSCREEN to MAXIMIZED     | Browser window controls must be detached from DOM
 */
 /* === IGNORE === */
 chrome.runtime.onMessageExternal.addListener(
@@ -18,6 +22,13 @@ chrome.runtime.onMessageExternal.addListener(
             // }
         /* === IGNORE === */
 
+        const screenConfig = {
+            left: 0,
+            width: Math.ceil(sender.tab.width / 2), 
+            top: 0,
+            height: Math.ceil(sender.tab.height / 2) 
+        }
+
         chrome.windows.getCurrent(function(currentWindow){
             /* === IGNORE === */
                 ///* sender.tab.windowId === currentWindow.id */
@@ -26,13 +37,41 @@ chrome.runtime.onMessageExternal.addListener(
             /* === IGNORE === */
 
             switch (true) {
-                // if currentWindow.state not maximized, but we pressing maximized button from webpage, please do the following:
+                
+                /* [START] TOGGLEABLE FULLSCREEN */
                 case ((currentWindow.state === 'maximized') && (message.WindowState === 'maximized')):
-                    chrome.windows.update(currentWindow.id, { state: 'fullscreen' })
+                    chrome.windows.update(currentWindow.id, { state: 'fullscreen' /* := .requestFullscreen() */ })
                     break;
-                case ((currentWindow.state == 'fullscreen') && (message.WindowState === 'maximized')):
-                    chrome.windows.update(currentWindow.id, { state: 'maximized' })
+                case (currentWindow.state && (message.WindowState === 'maximized')):
+                    // chrome.windows.update(currentWindow.id, { state: message.WindowState /* := .exitFullscreen() */ })
+                    if (false);
+                    else if (currentWindow.state === 'fullscreen'){
+                        chrome.windows.update(currentWindow.id, { state: message.WindowState /* := .exitFullscreen() */ })
+                    }
+                    else if (currentWindow.state === 'normal'){
+                        chrome.windows.update(currentWindow.id, { state: message.WindowState /* := .exitFullscreen() */ })
+                    }
+                    else;
+                    chrome.windows.update(currentWindow.id, { state: message.WindowState /* := .exitFullscreen() */ })
                     break;
+                /* TOGGLEABLE FULLSCREEN [END]  */
+
+                /* [START] INTERMEDIATE MINIMIZING STATE MANAGEMENT */
+                case ((currentWindow.state === 'fullscreen' || 'maximized') && (message.WindowState === 'minimized')):
+                    chrome.windows.update(currentWindow.id, { state: 'normal' , ...screenConfig})
+                    break;
+                case ((currentWindow.state === 'normal') && (message.WindowState === 'minimized')):
+                    console.log("KILMER")
+                    chrome.windows.update(currentWindow.id, { state: message.WindowState})
+                    break;
+                /* INTERMEDIATE MINIMIZING STATE MANAGEMENT [END] */
+                
+
+                /* [START] WINDOW CLOSING */
+                case (currentWindow.state && (message.WindowState === 'closed')):
+                    chrome.windows.remove(currentWindow.id /* := window.close() */)
+                    break;
+                /* WINDOW CLOSING [END] */
             }
 
     })
