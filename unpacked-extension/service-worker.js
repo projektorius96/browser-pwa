4ae89709a88fb3051ae84389df8eaa0e67b193a3
LG <34185919+projektorius96@users.noreply.github.com>
2024-01-13 15:20:26 +0200

    let's start from scratch...

--- unpacked-extension/service-worker.js
+++ unpacked-extension/service-worker.js
@@ -1,43 +1,34 @@
+/* === IGNORE === */
+    // chrome.runtime.onMessageExternal.addListener(
+    //     function (message, sender, sendResponse) {
+    //         chrome.windows.update(sender.tab.windowId, { state: message.WindowState })
+    //         otherwise do /* chrome.windows.remove(currentWindow.id) */
+    //     }
+    // );
+/* === IGNORE === */
+
+/* CONSIDERATIONS
+....................    | ............... | currentWindow.state
+----------------------------------------------------------------|
+IF CURRENTLY WINDOW IS  |   MINIMIZED     | "normal" (or "minimized" if .getCurrent was called from console)
+IF CURRENTLY WINDOW IS  |   MAXIMIZED     | "maximized"
+IF CURRENTLY WINDOW IS  |   FULLSCREEN    | "fullscreen"
+*/
 chrome.runtime.onMessageExternal.addListener(
     function (message, sender, sendResponse) {
-        switch (message.WindowState) {
-            case 'maximized':
-                chrome.windows.getCurrent(function (currentWindow) {
-                    chrome.windows.update(currentWindow.id, { state: message.WindowState });
-                    if (currentWindow.state !== "minimized"){
-                        chrome.windows.update(currentWindow.id, { state: 'fullscreen' });
-                        /* sendResponse({requestFullscreen: "requestFullscreen"}) */
-                    }
-                    // ALTERNATIVE B [FAILING] 
-                    // if (currentWindow.state === 'normal' || 'minimized'){
-                    //     chrome.windows.update(currentWindow.id, { state: 'fullscreen' });
-                    // }
-                    // else {
-                    //     chrome.windows.update(currentWindow.id, { state: 'normal' });
-                    // }
-                    // ALTERNATIVE C [FAILING]
-                    // if(false);
-                    // else if (currentWindow.state === message.WindowState){
-                    //     chrome.windows.update(currentWindow.id, { state: 'normal' });
-                    //     /* sendResponse({requestFullscreen: "requestFullscreen"}) */
-                    // }
-                    // else if (currentWindow.state !== message.WindowState){
-                    //     chrome.windows.update(currentWindow.id, { state: 'fullscreen' });
-                    //     /* sendResponse({requestFullscreen: "requestFullscreen"}) */
-                    // }
-                })
-            break;
-            case 'minimized':
-                chrome.windows.getCurrent(function (currentWindow) {
-                    chrome.windows.update(currentWindow.id, { state: message.WindowState });
-                })
-            break;
-            case 'closed':
-                chrome.windows.getCurrent(function (currentWindow) {
-                    // DEV_NOTE # it considers that whole application lifecycle manifests within single tab
-                    chrome.windows.remove(currentWindow.id)
-                });
-            break;
-        }
+        /* === IGNORE === */
+            // switch (message.WindowState) {
+            //     case 'normal':
+            //     case 'maximized':
+            //     case 'minimized':
+            //     case 'fullscreen':
+            //         console.log(message.WindowState)
+            // }
+        /* === IGNORE === */
+
+        chrome.windows.getCurrent(function(currentWindow){
+            console.log(currentWindow.state)
+        })
+
     }
 );
