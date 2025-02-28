82354e150a9370b0fe934bcfc47dd9320d55481c
LG <34185919+projektorius96@users.noreply.github.com>
2025-02-28 23:27:57 +0200

    Fullscreen mode works with one condition, see README

---
+++ README.md
@@ -0,0 +1,5 @@
+<h3>Fullscreen mode works with one condition</h3>
+
+<p>
+- Fullscreen must be request from browser's native UI for Pointer Lock API to take action: however, we will still be required to use Esc to bring back the native cursor to the screen
+</p>
--- main.js
+++ main.js
@@ -5,3 +5,17 @@ import { name } from './package.json';
 document.addEventListener('DOMContentLoaded', ()=>{
     document.title = name;
 })
+
+/* === */
+
+document.body.addEventListener('click', function(){
+    document.documentElement.requestFullscreen();
+    this.addEventListener('mousemove', (e)=>{
+        if (document.fullscreenElement && e.clientY <= 8) {
+            this.requestPointerLock() ;
+        }
+        if (document.pointerLockElement === this && e.clientY > 8){
+            document.exitPointerLock();
+        }
+    });
+});
